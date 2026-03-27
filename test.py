f75f53120c171927855c907737ceeb309da9df3d
Evgeny Styrov <estyrov@uma.tech>
2026-03-27 14:12:36 +0300

    date

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-from nautobot.apps.jobs import Job
+from nautobot.extras.jobs import Job
 
 class TestSSoT(Job):
     class Meta:
         name = "SSoT Test Job"
-        description = "Minimal test to verify jobs loading"
 
     def run(self):
         self.log_info(message="SSoT is working!")
