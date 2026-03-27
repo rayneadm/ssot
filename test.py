4e5c01ae984a5d90190f8471fb4b9e457e532c4d
Evgeny Styrov <estyrov@uma.tech>
2026-03-27 17:48:38 +0300

    date

--- test.py
+++
@@ -1,8 +0,0 @@
-from nautobot.extras.jobs import Job
-
-class TestSSoT(Job):
-    class Meta:
-        name = "SSoT Test Job"
-
-    def run(self):
-        self.log_info(message="SSoT is working!")
