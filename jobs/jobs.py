0f2f861960466dcd9fc83dd43eb09591db489198
Evgeny Styrov <estyrov@uma.tech>
2026-03-27 19:26:23 +0300

    date

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -1,40 +1,9 @@
-from nautobot.extras.jobs import Job, ObjectVar, register_jobs
-from nautobot.extras.models import ExternalIntegration
-from nautobot.extras.choices import (
-    SecretsGroupAccessTypeChoices,
-    SecretsGroupSecretTypeChoices,
-)
-
-import requests
+from nautobot.extras.jobs import Job, register_jobs
 
 
-class NetBoxTestJob(Job):
-
-    source = ObjectVar(
-        model=ExternalIntegration,
-        label="NetBox Instance",
-        required=True
-    )
-
-    class Meta:
-        name = "NetBox Test API"
-
-    def run(self, source):
-        url = source.remote_url
-
-        token = source.secrets_group.get_secret_value(
-            access_type=SecretsGroupAccessTypeChoices.TYPE_HTTP,
-            secret_type=SecretsGroupSecretTypeChoices.TYPE_TOKEN,
-        )
-
-        response = requests.get(
-            f"{url}/api/status/",
-            headers={"Authorization": f"Token {token}"},
-            timeout=10,
-        )
-
-        self.logger.info(response.status_code)
+class TestJob(Job):
+    def run(self):
+        self.logger.info("OK")
 
 
-# 🔥 обязательно
-register_jobs(NetBoxTestJob)
+register_jobs(TestJob)
